258021877c0a0ba3f03b7450c272193c68c0affe
Sebastian Wessalowski <sebastian@wessalowski.org>
2014-11-12 09:26:56 +0100

    Die sollten hier nicht sein

--- rtorrent.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-tmux new-session rtorrent \; detach-client
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-benutzer=(amboss dctmz neo proggi reaper roby techfreak kirschbluete)
-for i in "${benutzer[@]}"; do
-	su -c rtorrent.sh $i
-done
